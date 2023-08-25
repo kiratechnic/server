970a1bbc4eba6976107da89b4c18bebe95db8a69
Kirill <106878520+kiratechnic@users.noreply.github.com>
2023-08-25 20:12:35 +0300

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -1,3 +1,31 @@
+<form method='post'>
+  <input type='number' name='n'>
+  <input type='submit' name="bt_enter" value="Enter">
+</form>
 <?php
 
-echo "гыыы ;)";
+if(isset($_POST['bt_enter'])){
+  $n = $_POST['n'];
+
+  if($n == 0){
+    echo 'n!=1';
+  }
+  else{
+    $f = 1;
+    $i = $n;
+    while($i>0){
+      $f = $f * $i;
+      $i = $i - 1;
+    }
+    echo "n!=$f";
+  }
+
+  echo '<br>';
+  print('n * 5 = ' . $n*5);
+
+  echo '<br>';
+  while($n > 0){
+    echo 'Hello world! ';
+    $n = $n - 1;
+  }
+}
